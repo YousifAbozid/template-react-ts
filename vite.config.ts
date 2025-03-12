404f68015dc013db24f8dc15772ebe3abb1a90d4
Yousif Abozid <yousif.abozid@yahoo.com>
2025-03-12 23:19:25 +0200

    feat: add Tailwind CSS and PostCSS configuration, enhance Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: true,
+  },
 });

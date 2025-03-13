9b56489a8d068b3ac5e3eaa954e44bb82dea7f0d
Yousif Abozid <yousif.abozid@yahoo.com>
2025-03-13 15:06:53 +0200

    feat: refactor color variables and update App component for improved dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-white dark:bg-dark-background-primary text-black dark:text-white">
+    <div className="h-screen flex flex-col justify-center items-center bg-l-bg-1 dark:bg-d-bg-1 text-l-text-1 dark:text-d-text-1">
       <h1 className="text-3xl font-bold">Hello, Tailwind Dark Mode!</h1>
       <ThemeToggle />
     </div>

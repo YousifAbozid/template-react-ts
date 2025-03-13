f232efb1484d601bb24b901bf05a275317997443
Yousif Abozid <yousif.abozid@yahoo.com>
2025-03-13 14:59:50 +0200

    fix: update dark mode background color for improved visibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-white dark:bg-black text-black dark:text-white">
+    <div className="h-screen flex flex-col justify-center items-center bg-white dark:bg-dark-background-primary text-black dark:text-white">
       <h1 className="text-3xl font-bold">Hello, Tailwind Dark Mode!</h1>
       <ThemeToggle />
     </div>
